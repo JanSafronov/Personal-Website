a687ce793825ccf240655622e5411aef9657bf91
Pomidorka1234 <Jansafronov@gmail.com>
2021-05-30 16:17:41 +0300

    Commit GitHub href

--- Personal Website/wwwroot/js/Dev.js
+++ Personal Website/wwwroot/js/Dev.js
@@ -37,7 +37,9 @@ class Repository extends React.Component {
                     <h4>{this.props.main}</h4>
                 </div>
                 <div id={"repos-fig-" + this.props.ID} className="repos-fig">
-
+                    <div id={"fig-details-" + this.props.ID} className="fig-details">
+                        <h6>GitHub: <a href={this.props.Href}>Repository</a></h6>
+                    </div>
                 </div>
             </div>
         );
@@ -49,21 +51,24 @@ ReactDOM.render(function () {
         title: "'ValueTB' Application",
         status: "In process",
         type: "Windows Application",
-        main: "WPF desktop application equipped with table management and calculation of values and transformation of data and import of various table formats."
+        main: "WPF desktop application equipped with table management and calculation of values and transformation of data and import of various table formats.",
+        href: "https://github.com/Pomid0rchik/ValueTB"
     }, {
         title: "'Online Dish' Website",
         status: "In process",
         type: "ASP.NET Website",
-        main: "ASP.NET Full stack web project template of a food delivery company."
+        main: "ASP.NET Full stack web project template of a food delivery company.",
+        href: ""
     }, {
         title: "'DebunkInfo' Website",
         status: "Not deployed",
         type: "ASP.NET Framework Website",
-        main: "ASP.NET Framework project full stack presentation repository of a fact checking and user interactive website."
+        main: "ASP.NET Framework project full stack presentation repository of a fact checking and user interactive website.",
+        href: "https://github.com/Pomid0rchik/DebunkInfo-WebSite-Presentation"
     }];
     let r = [];
     for (let i = 0; i < 3; i++) {
-        r.push(<Repository ID={i} title={l[i].title} status={l[i].status} type={l[i].type} main={l[i].main} />);
+        r.push(<Repository ID={i} title={l[i].title} status={l[i].status} type={l[i].type} main={l[i].main} Href={l[i].href}/>);
     }
     return r;
 }(), document.getElementById("main-container"));
