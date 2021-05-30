df5d68dc7674b5e1746c6d4cd4a88e42e2d9b92a
Pomidorka1234 <Jansafronov@gmail.com>
2021-05-30 16:08:05 +0300

    Commit short

--- Personal Website/wwwroot/js/Index.js
+++ Personal Website/wwwroot/js/Index.js
@@ -7,7 +7,6 @@ var pastscroll = 0;
 window.addEventListener("scroll", function (e) {
     let float = (this.scrollY > pastscroll ? -0.01 : 0.01);
     pastscroll = this.scrollY;
-    //alert("scrolled");
     if ($("body").css("width") < "225px")
         $("#image-topcenter").css("opacity", (parseFloat($("#image-topcenter").css("opacity")) + float).toString());
 });
